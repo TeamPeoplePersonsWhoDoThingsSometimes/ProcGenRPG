1e05ce569c814f845d7652be3ee8785a0de070ac
kkiniaes <kkiniaes@gmail.com>
2014-11-12 22:34:00 -0500

    Revert "Revert "Implemented Chests""
    This reverts commit a8fbadefc99e32fbe28b82eb526aeb476e20ef3d.

---
+++ Assets/Scripts/MapGen/Chest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Chest : MonoBehaviour {
+
+	private static readonly int SLOTS = 4;
+	private List<Item> items;
+
+	// Use this for initialization
+	void Start () {
+		items = new List<Item>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool addToChest(Item i) {
+		if (items.Count < SLOTS) {
+			items.Add(i);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public bool removeFromChest(Item i) {
+		return items.Remove(i);
+	}
+
+	public List<Item> getList() {
+		return items;
+	}
+}
