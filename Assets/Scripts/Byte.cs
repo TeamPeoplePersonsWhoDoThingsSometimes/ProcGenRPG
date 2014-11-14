c4f5c31fc52eb205b78f1eb07f64135462629c85
kkiniaes <kkiniaes@gmail.com>
2014-11-14 00:35:02 -0500

    Modified byte behavior

--- Assets/Scripts/Byte.cs
+++ Assets/Scripts/Byte.cs
@@ -27,7 +27,7 @@ public class Byte : MonoBehaviour {
 			rigidbody.AddForce(new Vector3(Random.value*8f - 4, Random.value*2f, Random.value*8f - 4), ForceMode.VelocityChange);
 		}
 
-		if ((timeOffset > 1 && Vector3.Distance(Player.playerPos.position, this.transform.position) < 10f) || timeOffset > 20) {
+		if ((timeOffset > 1 && Vector3.Distance(Player.playerPos.position, this.transform.position) < 10f) || timeOffset > 2) {
 			this.transform.position = Vector3.MoveTowards(this.transform.position, Player.playerPos.position + new Vector3(0,1,0),20*Time.deltaTime);
 			this.rigidbody.useGravity = false;
 			this.collider.enabled = false;
