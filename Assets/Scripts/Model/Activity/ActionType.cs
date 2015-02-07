f898c73eed559c7e688619d2287c96bd32d6b9d5
William Johnson <johnsonalex1@gatech.edu>
2015-02-07 12:46:14 -0500

    protobuf initial additions

--- Assets/Scripts/Model/Activity/ActionType.cs
+++ Assets/Scripts/Model/Activity/ActionType.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿//ActionTypes are now included in the protobuf definition in order to unify the builder and engine
+//either change things in the .proto file in the builder repository or shoot a project lead an email.
+//you could also post to the groupme and I'll see it there and make the changes.
+
+/**using UnityEngine;
 using System.Collections;
 
 public enum ActionType {
@@ -6,3 +10,4 @@ public enum ActionType {
 	MOVE_AREA,
 	APPROACHED_OBJECT
 }
+*/
