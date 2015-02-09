208cc8ed6c8b0909d39d5af9f8a7de25c130542e
James Lee <wisdom0music@gmail.com>
2015-02-09 16:39:04 -0500

    A new power! And weapon fixes
    Made a new hack called Force Push to coincide with my lightsaber. Also
    fixed the positions of the weapons.

---
+++ Assets/Scripts/Combat/Hacks/ForcePush.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForcePush : Hack {
+
+	protected override void OneShotActivated ()
+	{
+		base.OneShotActivated ();
+//		GameObject tempAttack = (GameObject)Instantiate(attack, Player.playerPos.position + ((attack.transform.localScale.x/2f)*Player.playerPos.right), Quaternion.LookRotation(Player.playerPos.right));
+		GameObject tempAttack = (GameObject)Instantiate(attack, Player.playerPos.position, Player.playerPos.rotation);
+		tempAttack.GetComponent<Attack>().SetDamage(damage);
+		tempAttack.GetComponent<Attack>().SetCrit(critChance);
+	}
+}
