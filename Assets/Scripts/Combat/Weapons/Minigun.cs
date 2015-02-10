c84b6b5d58332cb21ec1e0ea1ae9d00aa194c1af
James Lee <wisdom0music@gmail.com>
2015-02-09 19:44:13 -0500

    Another weapon?! YES. plus visual enhancements
    FINALLY, i do the thing i put myself on the tasklist for. Behold le
    flamethrower.
    
    Also i added a bullet visual to the minigun. PURELY aesthetic to make it
    have a "dakka" feel :)

---
+++ Assets/Scripts/Combat/Weapons/Minigun.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minigun : Weapon {
+
+	public GameObject bulletParticles;
+
+	public override void Attack (float damage)
+	{
+		//GameObject.Instantiate(bulletParticles, transform.position + new Vector3(0,0,3), Player.playerPos.rotation);
+		GameObject.Instantiate(bulletParticles, Player.playerPos.position + new Vector3(0,2.5f,0), Player.playerPos.rotation);
+
+		RaycastHit hitInfo;
+		if(Physics.Raycast(new Ray(Player.playerPos.position + Player.playerPos.forward + new Vector3(0,1,0),Player.playerPos.forward), out hitInfo)) {
+			if(hitInfo.collider.gameObject.GetComponent<Enemy>() != null) {
+				Enemy temp = hitInfo.collider.gameObject.GetComponent<Enemy>();
+				temp.GetDamaged(damage, Random.value < critChance);
+				temp.DoKnockback(hitInfo.point, knockback);
+
+				if(attackOBJ != null) {
+					if(Random.value < attackOBJ.GetComponent<Attack>().attackEffectChance) {
+						temp.GetDamaged(attackOBJ.GetComponent<Attack>().attackEffect,
+						                attackOBJ.GetComponent<Attack>().attackEffectValue,
+						                attackOBJ.GetComponent<Attack>().attackEffectTime);
+					}
+				}
+			}
+		}
+
+	}
+
+}
