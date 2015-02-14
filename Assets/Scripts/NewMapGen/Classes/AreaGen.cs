e4c8996b9321addf84ab16f8c81dbd19a3c409e7
Ben Bohannon <benbo135@live.com>
2015-02-14 09:43:57 -0500

    Started A* corridor making.
    A work in progress. Currently does not work at all.

--- Assets/Scripts/NewMapGen/Classes/AreaGen.cs
+++ Assets/Scripts/NewMapGen/Classes/AreaGen.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using PriorityQueue;
 
 public static class AreaGen {
 
@@ -23,6 +24,8 @@ public static class AreaGen {
 
         //Do A* to connect each room.
         corridors = defaultConnect(seed, ref tiles, ref rooms);
+
+        placeCorridors(ref tiles, corridors);
     }
 
     private static void defaultRoomGen(int seed, ref TileData[,] tiles, ref List<Room> rooms)
@@ -77,18 +80,46 @@ public static class AreaGen {
         }
     }
 
+    //Creates corridors from room to room,and returns the list of corridors. (Rooms tiles are not included in corridors).
     private static List<TileData> defaultConnect(int seed, ref TileData[,] tiles, ref List<Room> rooms)
     {
         //TODO: Implement this method.
 
         //Generate the weight map for A*.
-        int[,] weights = generateWeight(seed, tiles.GetLength(0), tiles.GetLength(1));
+        int[,] weights = generateWeight(seed, tiles);
 
-        //TODO: Do A* connecting of Rooms.
+        List<TileData> corridors = new List<TileData>();
 
-        return null;
+        //TODO: Choose Rooms to connect.
+        for (int i = 1; i < rooms.Count; i++)
+        {
+
+            //Plan:
+            //Add starting point to Open List.
+            //Sort Open list by F value, which is the distance from the end (found by heuristic) + currentCost.
+            //While Open List is NOT empty
+                //Get next Point to calculate, and put it on closed list.
+                //foreach neighbor to this nextPoint
+                    //If next point is the final point, make connections and break.
+                    //If next point is not walkable, continue.
+                    //If next point is NOT on the open List, calc ALL it's moveCost values F,G,H, and set the nextPoint
+                        //as it's cameFrom point.
+                    //If this point is ON the open List (and maybe the closed),
+                        //if this REALcost (G, not heuristic) from this nextPoint is better than
+                        //the one it already has, replace its cameFrom with this point, and re-calc its F,G,H
+
+            //OpenList sorted by F values.
+            PriorityQueue<int, path> openList = new PriorityQueue<int, path>();
+
+
+        }
+
+        return corridors;
     }
 
+    #region Helper Methods
+
+    //Puts the input Room on the map as a TileData object.
     private static void placeRoom(ref TileData[,]tiles, Room r)
     {
         Point botLeft = r.getBotLeft();
@@ -105,8 +136,11 @@ public static class AreaGen {
     }
 
     //Generates a randomized 2D int Array, containing numbers 1-5.
-    private static int[,] generateWeight(int seed, int length, int height)
+    private static int[,] generateWeight(int seed, TileData[,] tiles)
     {
+        int length = tiles.GetLength(0);
+        int height = tiles.GetLength(1);
+
         System.Random random = new System.Random(seed);
         int[,] weights = new int[length, height];
 
@@ -114,10 +148,54 @@ public static class AreaGen {
         {
             for(int j = 0; j < height; j++)
             {
-                weights[i, j] = random.Next(1, 5);
+                if (tiles[i, j] == null)
+                {
+                    weights[i, j] = random.Next(2, 5);
+                }
+                else
+                {
+                    //If this is part of a Room, then set its weight to one, to prefer already existing paths.
+                    weights[i, j] = 0;
+                }
             }
         }
 
         return weights;
+    }
+
+    //Iterates through the list of Corridors and places them all on the tile map.
+    private static void placeCorridors(ref TileData[,] tiles, List<TileData> corridors)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    #endregion
+
+    //Internal class used to help generate corridors.
+    internal class path
+    {
+        int cost;         //G
+        int estDist;      //H
+        int probableCost; //F = G + H
+
+        Point cameFrom;
+
+        bool closedList = false;
+
+        internal path(Point cameFrom)
+        {
+            this.cameFrom = cameFrom;
+        }
+
+        //Returns an array of the neighbors of this path.
+        public path[] getNeighbors()
+        {
+            return null;
+        }
+
+        public Point getCameFrom()
+        {
+            return cameFrom;
+        }
     }
 }
