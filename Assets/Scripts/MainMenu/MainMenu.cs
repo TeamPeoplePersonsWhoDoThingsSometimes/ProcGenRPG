45c218765765d68f8f27c7dc86aaa5a460a0050f
kkiniaes <kkiniaes@gmail.com>
2015-02-20 11:22:32 -0500

    Added main menu rough draft + armor prefabs

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -5,14 +5,18 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour {
 
 	private enum MenuState {Splash, Main, LoadSave, Options, Credits};
+	private bool transitioningToMain;
 
 	private Image dotBG;
 	private int dotBGCounter1;
 	private MenuState curState = MenuState.Splash;
+	private GameObject splashContainer, mainContainer;
 
 	// Use this for initialization
 	void Start () {
 		dotBG = GameObject.Find("DOTBGPREFAB").GetComponent<Image>();
+		splashContainer = GameObject.Find("SplashContainer");
+		mainContainer = GameObject.Find("MainContainer");
 	}
 	
 	// Update is called once per frame
@@ -20,16 +24,35 @@ public class MainMenu : MonoBehaviour {
 		switch(curState) {
 
 		case MenuState.Splash:
-			if(dotBGCounter1 < 25) {
-				for(int i = 0; i < 33; i++) {
-					Image temp = (Image)Instantiate(dotBG,dotBG.transform.position, Quaternion.identity);
-					temp.transform.parent = dotBG.transform.parent;
-					temp.rectTransform.anchoredPosition = new Vector2(i*25 - 400, 300 - dotBGCounter1*25);
-					temp.enabled = true;
+			if(Time.frameCount % 2 == 0) {
+				if(dotBGCounter1 < 25) {
+					for(int i = 0; i < 33; i++) {
+						Image temp = (Image)Instantiate(dotBG,dotBG.transform.position, Quaternion.identity);
+						temp.rectTransform.SetParent(dotBG.transform.parent);
+						temp.rectTransform.anchoredPosition = new Vector2(i*25 - 400, 300 - dotBGCounter1*25);
+						temp.enabled = true;
+					}
+					dotBGCounter1++;
 				}
-				dotBGCounter1++;
+			}
+			if(dotBGCounter1 >= 25) {
+				if(Input.anyKey) {
+					transitioningToMain = true;
+				}
+				if(transitioningToMain) {
+					TransitionToMain();
+				}
+			}
+			mainContainer.SetActive(false);
+			splashContainer.SetActive(true);
+			break;
+		case MenuState.Main:
+			mainContainer.SetActive(true);
+			splashContainer.SetActive(false);
+			if(Camera.main.GetComponent<ChromaticAbberation>().scale > 2) {
+				Camera.main.GetComponent<ChromaticAbberation>().scale--;
 			} else {
-
+				Camera.main.GetComponent<ChromaticAbberation>().scale = 0.3f;
 			}
 			break;
 
@@ -38,6 +61,9 @@ public class MainMenu : MonoBehaviour {
 	}
 
 	void TransitionToMain() {
-
+		Camera.main.GetComponent<ChromaticAbberation>().scale++;
+		if(Camera.main.GetComponent<ChromaticAbberation>().scale > 30) {
+			curState = MenuState.Main;
+		}
 	}
 }
--- Assets/Scripts/MainMenu/ResponsiveDot.cs
+++ Assets/Scripts/MainMenu/ResponsiveDot.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class ResponsiveDot : MonoBehaviour {
-
+	
 	// Use this for initialization
 	void Start () {
 	
@@ -11,8 +11,19 @@ public class ResponsiveDot : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 temp = Mathf.Min(2f,Mathf.Max(200f/(Vector3.Distance(transform.position, new Vector3(Camera.main.ScreenToViewportPoint(Input.mousePosition).x*Screen.width, Camera.main.ScreenToViewportPoint(Input.mousePosition).y*Screen.height))+1),0.5f)) * Vector3.one;
-		transform.localScale = new Vector3(temp.x, temp.y, temp.z);
-//		Debug.Log (1f/(Vector3.Distance(transform.position, new Vector3(Camera.main.ScreenToViewportPoint(Input.mousePosition).x*Screen.width, Camera.main.ScreenToViewportPoint(Input.mousePosition).y*Screen.height))+1));
+		if(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("New State")) {
+			Vector3 mousePos = Camera.main.ViewportToWorldPoint(new Vector3(Input.mousePosition.x/Screen.width, Input.mousePosition.y/Screen.height, transform.position.z));
+			Vector3 temp = Mathf.Min(4f,Mathf.Max(50f/(Vector3.Distance(transform.position,new Vector3(mousePos.x, mousePos.y, transform.position.z))+1),2f)) * Vector3.one;
+			transform.localScale = new Vector3(temp.x, temp.y, temp.z);
+			GetComponent<RectTransform>().sizeDelta = new Vector2(1f,1f);
+		}
+
+
+		if(Mathf.Abs(transform.position.x - 102.7f) < 2f && Mathf.Abs(transform.position.y - 51.3f) < 2f) {
+//			Debug.Log(200f/(Vector3.Distance(transform.position, new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x*Screen.width, Camera.main.ScreenToWorldPoint(Input.mousePosition).y*Screen.height, transform.position.z))+1) * Vector3.one);
+//			Debug.Log(Camera.main.ViewportToWorldPoint(new Vector3(Input.mousePosition.x/Screen.width, Input.mousePosition.y/Screen.height, transform.position.z))*4.5f);
+//			Debug.Log(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime);
+//			Debug.Log(transform.localScale);
+		}
 	}
 }
